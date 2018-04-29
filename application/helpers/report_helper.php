e601ae7c2d31b0abe3f4c0f36dc4db38a19d07ba
fajarrdp <fajarrdp@gmail.com>
2018-04-30 04:33:17 +0700

    Update

--- application/helpers/report_helper.php
+++ application/helpers/report_helper.php
@@ -4,4 +4,22 @@ function get_header_fields() {
     return ["KLOBS","KL15","BBLS","LONGTON","METRICTON"];
 }
 
+function get_split_element($str_fields='') {
+    // sample
+    // $str_fieldsex = '["SL_VS_BOL_R1_KLOBS|KLOBS","SL_VS_BOL_R1_KL15|KL15","SL_VS_BOL_R1_BBLS|BBLS","SL_VS_BOL_R1_LONGTON|LONGTON","SL_VS_BOL_R1_METRICTON|METRICTON","SL_VEF_APPLIED_VS_BOL_KLOBS|KLOBS","SL_VEF_APPLIED_VS_BOL_KL15|KL15","SL_VEF_APPLIED_VS_BOL_BBLS|BBLS","SL_VEF_APPLIED_VS_BOL_LONGTON|LONGTON","SL_VEF_APPLIED_VS_BOL_METRICTON|METRICTON"]';
+    $str_fields = json_decode($str_fieldsex);
+    $array_key = array();
+    foreach($str_fields as $k => $v) {
+        $explode = explode("|",$v);
+        // search key
+        if(!isset($array_key[$explode[1]])) {
+            $array_key[$explode[1]] = array($explode[0]);
+        } else {
+            array_push($array_key[$explode[1]],$explode[0]);
+        }
+    }
+
+    return $array_key;
+}
+
 ?>
