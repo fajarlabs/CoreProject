fea1e153823ab2877b304d69a5cec88fc37df733
fajarrdp <fajarrdp@gmail.com>
2018-04-30 07:59:22 +0700

    Update

--- application/helpers/report_helper.php
+++ application/helpers/report_helper.php
@@ -6,8 +6,6 @@ function get_header_fields() {
 
 // fungsinya untuk memilah menjadi 1 kategori
 function get_split_element($str_fields='') { 
-    // sample
-    // $str_fieldsex = '["SL_VS_BOL_R1_KLOBS|KLOBS","SL_VS_BOL_R1_KL15|KL15","SL_VS_BOL_R1_BBLS|BBLS","SL_VS_BOL_R1_LONGTON|LONGTON","SL_VS_BOL_R1_METRICTON|METRICTON","SL_VEF_APPLIED_VS_BOL_KLOBS|KLOBS","SL_VEF_APPLIED_VS_BOL_KL15|KL15","SL_VEF_APPLIED_VS_BOL_BBLS|BBLS","SL_VEF_APPLIED_VS_BOL_LONGTON|LONGTON","SL_VEF_APPLIED_VS_BOL_METRICTON|METRICTON"]';
     $str_fields = json_decode($str_fields);
     $array_key = array();
     if(count($str_fields) > 0) :
